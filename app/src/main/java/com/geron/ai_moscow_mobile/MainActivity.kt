27d1994602301aec761285c4245f0445ddd2f5bc
GerONSo <sergon2002@gmail.com>
2021-06-11 23:01:59 +0300

    init app

---
+++ app/src/main/java/com/geron/ai_moscow_mobile/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.geron.ai_moscow_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
