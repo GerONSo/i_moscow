8696da290e9e9de89df9faf1fa57006480c9c265
yemifino <stepansteepan@gmail.com>
2021-06-12 19:41:33 +0200

    test

--- app/src/main/java/com/geron/ai_moscow_mobile/MainActivity.kt
+++ app/src/main/java/com/geron/ai_moscow_mobile/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         openFragment(startFragment)
         setCallbacks()
     }
-//test
+//tewse
     private fun setCallbacks() {
         CallbackHelper.onEventItemClicked = { position ->
             openFragment(EventFragment(position))
